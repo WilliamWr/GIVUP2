292927a43e75ccb81b76515ad5e60769328aeaf8
Mac User <youcanreach_will@icloud.com>
2017-07-31 19:32:47 -0400

    fixed last profile glitch

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,8 @@ class Event < ApplicationRecord
   geocoded_by :full_street_address   # can also be an IP address
   # has_many :users, through: :user_events
 
+  def hours(user_id)
+    return self.user_events.find_by(user_id: user_id).hours || 0
+  end
+
 end
